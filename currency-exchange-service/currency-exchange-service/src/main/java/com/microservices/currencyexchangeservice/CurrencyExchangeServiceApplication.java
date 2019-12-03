a49bb0965a30d80a66992671c54680853a82e1b3
Mahajan, Rajat (VW IT Services India) <rajat.mahajan@volkswagen.co.in>
2019-12-03 19:12:26 +0530

    first commit

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,17 @@
+package com.microservices.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
